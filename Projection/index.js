351bfc5d8f5a7e49da6f9bdec94d3b1bfc64b53f
loelu <lorenz.vonlanthen@students.ffhs.ch>
2021-03-26 17:24:54 +0100

    Working version

--- Projection/index.js
+++ Projection/index.js
@@ -1,5 +1,9 @@
+require("babel-core/register");
+require("babel-polyfill");
+require('@tensorflow/tfjs-backend-cpu');
 const Plotly = require('plotly.js-dist');
 const faceLandmarksDetection = require('@tensorflow-models/face-landmarks-detection');
+const tfData = require('@tensorflow/tfjs-data');
 
 // If you are using the WebGL backend:
 require('@tensorflow/tfjs-backend-webgl');
@@ -9,52 +13,79 @@ let webcam
 let webcamElement = document.getElementById("webcam")
 let capturing = false
 
+const canvas = document.getElementById('plottingCanvas');
+const ctx = canvas.getContext('2d');
+
 async function capture() {
     capturing = true
 
     while (capturing) {
         const img = await webcam.capture();
-        const predictions = await model.estimateFaces(img);
-
+        const predictions = await model.estimateFaces({input: img});
+        console.log(predictions)
         if (predictions.length > 0) {
-           
-            let a = []; b = []; c = []
+            ctx.globalCompositeOperation = 'destination-over';
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
             for (let i = 0; i < predictions.length; i++) {
-                const keypoints = predictions[i].mesh;
-                // Log facial keypoints.
-                for (let i = 0; i < keypoints.length; i++) {
-                    const [x, y, z] = keypoints[i];
-                    a.push(y)
-                    b.push(x)
-                    c.push(z)
-                }
+                const keypoints = predictions[i].annotations;
+                // silhouette
+                drawObject(keypoints.silhouette);
+
+                // left eye
+                const leftEyeKeypoints = keypoints.leftEyeLower0.concat(keypoints.leftEyeUpper0.reverse())
+                drawObject(leftEyeKeypoints);
+
+                // left eyebrow
+                const leftEyeBrowKeypoints = keypoints.leftEyebrowLower.concat(keypoints.leftEyebrowUpper.reverse())
+                drawObject(leftEyeBrowKeypoints);
+
+                // right eye
+                const rightEyeKeypoints = keypoints.rightEyeLower0.concat(keypoints.rightEyeUpper0.reverse())
+                drawObject(rightEyeKeypoints);
+
+                // right eyebrow
+                const rightEyeBrowKeypoints = keypoints.rightEyebrowLower.concat(keypoints.rightEyebrowUpper.reverse())
+                drawObject(rightEyeBrowKeypoints);
+
+                // lips inner
+                const lipsInnerKeypoints = keypoints.lipsLowerInner.concat(keypoints.lipsUpperInner.reverse())
+                drawObject(lipsInnerKeypoints);
+
+                // lips outer
+                const lipsOuterKeypoints = keypoints.lipsLowerOuter.concat(keypoints.lipsUpperOuter.reverse())
+                drawObject(lipsOuterKeypoints);
+
+                // nose line
+                const noseLineKeypoints = keypoints.midwayBetweenEyes.concat(keypoints.noseTip)
+                drawObject(noseLineKeypoints);
+
+                // nose
+                const noseKeypoints = keypoints.noseLeftCorner.concat(keypoints.noseBottom).concat(keypoints.noseRightCorner)
+                drawObject(noseKeypoints);
             }
-
-            // Plotting the mesh
-            var data = [
-                {
-                    opacity: 0.8,
-                    color: 'rgb(300,100,200)',
-                    type: 'mesh3d',
-                    x: a,
-                    y: b,
-                    z: c,
-                }
-            ];
-            Plotly.newPlot('plot', data)
-
         }
     }
 }
 
+function drawObject(keypoints) {
+    ctx.beginPath();
+    for (let i = 0; i < keypoints.length; i++) { 
+        keypoint = keypoints[i];
+        if (i==0) {
+            ctx.moveTo(keypoint[0], keypoint[1]);
+        }
+        ctx.lineTo(keypoint[0], keypoint[1]);
+    }
+    ctx.closePath();
+    ctx.stroke();
+}
+
 async function main() {
-    // Load the MediaPipe facemesh model.
-    // model = await facemesh.load();
     model = await faceLandmarksDetection.load(
         faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);    
-    console.log("Model loaded")
 
-    webcam = await tf.data.webcam(webcamElement);
+    webcam = await tfData.webcam(webcamElement);
     const imgtemp = await webcam.capture();
     imgtemp.dispose()
 
